982c90e19b60cd4d6f6d1c6eed789a753792d2c2
linh97abc <linh97abc@gmail.com>
2022-03-21 00:55:36 +0700

    first

---
+++ gcov.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2018 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DEBUG_GCOV_H_
+#define ZEPHYR_INCLUDE_DEBUG_GCOV_H_
+
+void gcov_coverage_dump(void);
+void gcov_static_init(void);
+
+#endif	/* ZEPHYR_INCLUDE_DEBUG_GCOV_H_ */
